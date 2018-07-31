0c96c4921229e3402033521f03e1edec1db1bd07
Nicolas Shak <Nshak@Ucsc.edu>
2018-07-31 15:50:51 -0700

    routes test

--- routes.js
+++ routes.js
@@ -5,4 +5,8 @@ module.exports = function(app) {
 	app.get('/test', function(req, res) {
 		res.send('helloworld');
 	})
+
+	app.get('/', function(req, res) {
+		res.sendFile('explorer.html');
+	})
 }
