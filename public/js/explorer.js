a590dddd5637bb117989f97150ca812b19d15afb
Nicolas Shak <Nshak@Ucsc.edu>
2018-07-31 16:29:17 -0700

    changed url in server.js

--- public/js/explorer.js
+++ public/js/explorer.js
@@ -43,7 +43,7 @@ function buildFolder(name, date, contents) {
 window.onload = function() {
 
 	http = new XMLHttpRequest();
-	url = 'ec2-18-206-255-255.compute-1.amazonaws.com/test:3000';
+	url = 'http://ec2-18-206-255-255.compute-1.amazonaws.com/test:3000/';
 	http.open('GET', url, true);
 	http.send();
 
