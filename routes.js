b393fef727fd01159f62c418fda5188590933565
Nicolas Shak <Nshak@Ucsc.edu>
2018-08-05 15:40:20 -0700

    changed url in server.js

--- routes.js
+++ routes.js
@@ -4,9 +4,9 @@ module.exports = function(app) {
 
 	app.get('/test', function(req, res) {
 		res.send('helloworld');
-	})
+	});
 
 	app.get('/', function(req, res) {
 		res.sendFile(path.join(__dirname + '/explorer.html'));
-	})
+	});
 }
