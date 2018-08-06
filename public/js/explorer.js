8f59f96daf424eb9c08c8f388cc62ea03b0e6dcf
Nicolas Shak <Nshak@Ucsc.edu>
2018-08-05 17:47:24 -0700

    test

--- public/js/explorer.js
+++ public/js/explorer.js
@@ -48,6 +48,7 @@ window.onload = function() {
 	var html = 'This is wrong';
 	http.onreadystatechange = function() {
 		if(this.readyState == 4 && this.status == 200) {
+			console.log('Response Text: ' + this.responseText);
 			html = this.responseText;
 		}
 	};
